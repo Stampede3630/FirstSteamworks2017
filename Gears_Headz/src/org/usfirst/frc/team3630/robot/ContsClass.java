acd090aa0b7af409d2b8588634998268dd244e91
StampedeRobotics <breckstampede@gmail.com>
2017-02-11 11:34:16 -0600

    add homebrewMecanum calculations
    does not add talon cofigs yet. That is next. Added the matrix math sam
    worked out with Mr. Senner.

--- Gears_Headz/src/org/usfirst/frc/team3630/robot/ContsClass.java
+++ Gears_Headz/src/org/usfirst/frc/team3630/robot/ContsClass.java
@@ -4,7 +4,9 @@ public final class ContsClass {
 
 
 
-public static final int gearTalonpwmNumber = 0;
+public static final int gearTalonPWMPin = 0;
+
+public static final double mecanumPositionConstant = 0.625; //This is the length in meters of the distance from the center of the robot to the edge, and then the edge to the wheel. This is a positive number.
 
 
 }
---
+++ Gears_Headz/src/org/usfirst/frc/team3630/robot/HomebrewMecanum.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team3630.robot;
+
+public class HomebrewMecanum extends Robot {
+
+	public double[] mecanumDrive (float velocityX, float velocityY, float angularVelocityDeg ){
+		//This function takes the velocity in x and y and theta and converts them to 4 wheel speeds, from the top left counterclockwise.
+		double angularVelocityRad = angularVelocityDeg * Math.PI / 180;
+		double[] wheelspeedResult;
+		wheelspeedResult = new double[4];
+		//For more information about this formula, see the mecanum kinematics 
+		wheelspeedResult[0] = velocityX - velocityY -ContsClass.mecanumPositionConstant * angularVelocityRad;	
+		wheelspeedResult[1] = velocityX + velocityY - ContsClass.mecanumPositionConstant * angularVelocityRad;
+		wheelspeedResult[2] = velocityX - velocityY + ContsClass.mecanumPositionConstant * angularVelocityRad;
+		wheelspeedResult[3] = velocityX + velocityY + ContsClass.mecanumPositionConstant * angularVelocityRad;
+		
+		return wheelspeedResult;
+	}
+	//public void driveImplementation (float v1, float v2, float v3, float v4){}
+	
+	
+}
