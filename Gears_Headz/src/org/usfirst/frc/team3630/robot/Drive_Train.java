e4607c52cd4a17051e9a123e29f0a9d3480f4dbc
StampedeRobotics <breckstampede@gmail.com>
2017-02-11 14:46:29 -0600

    Revert "drive commit"
    This reverts commit b6c322de1e5ba0406baaf07e4397a63acfe22176.

--- Gears_Headz/src/org/usfirst/frc/team3630/robot/Drive_Train.java
+++ Gears_Headz/src/org/usfirst/frc/team3630/robot/Drive_Train.java
@@ -1,13 +1,8 @@
 package org.usfirst.frc.team3630.robot;
 
 
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Talon;
-
 public class Drive_Train extends Robot  {
 	Joystick m_Joystick;// 2 for shooting and driving 
  
@@ -25,31 +20,16 @@ public class Drive_Train extends Robot  {
     // initialize drives 
     RobotDrive m_robotDrive;
     
-    Talon robotFrontLeft;
-    Talon robotFrontRight;
-    Talon robotRearRight;
-    Talon   robotRearLeft;
+
+    
     public Drive_Train(){
-    robotFrontLeft= new Talon(0);
-     robotFrontRight= new Talon(3);
-     robotRearRight = new Talon(2);
-     robotRearLeft = new Talon(1);
-     
+    
     	m_Joystick= new Joystick(0);
-    	robotFrontLeft.setInverted(true);
-    	robotRearLeft.setInverted(true);
-    	m_robotDrive= new RobotDrive(robotFrontLeft,robotRearLeft,robotFrontRight,robotRearRight);
-    }
-public  void telopPeriodic() {
-	double slowDown = 3.0;
-	double xLeft = m_Joystick.getX();
-	double rightSticktwist = m_Joystick.getTwist();
-	double yRight =  m_Joystick.getY();
-			
-	double gyroAngle = 0;
-	m_robotDrive.mecanumDrive_Cartesian(xLeft, -yRight, - rightSticktwist, gyroAngle);
-	SmartDashboard.putNumber("xLeft", xLeft);
-	SmartDashboard.putNumber("yRight", yRight);
-	SmartDashboard.putNumber("yLeft", rightSticktwist);
-	}
+    	m_robotDrive= new RobotDrive(2,0,1,3);
+}
+
+public  void telopPeriodic(){
+	m_robotDrive.mecanumDrive_Cartesian(m_Joystick.getX()/5, m_Joystick.getTwist(), m_Joystick.getY(),0);
+	
+}
 }
