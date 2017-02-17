3b08f208ae60d0e477ebd679a3d766eb6dffe48a
unreal6 <snacklandserver@gmail.com>
2017-02-17 13:30:45 -0600

    updated drivetrain.java to get rid of github edits
    This should help update the master branch so that everything is properly formatted.

--- Gears_Headz/src/org/usfirst/frc/team3630/robot/Drive_Train.java
+++ Gears_Headz/src/org/usfirst/frc/team3630/robot/Drive_Train.java
@@ -1,25 +1,16 @@
 package org.usfirst.frc.team3630.robot;
 
 
-<<<<<<< HEAD
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Talon;
-
-public class Drive_Train extends Robot {
-=======
-
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.JoystickBase;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Drive_Train extends Robot  {
->>>>>>> origin/newmecanumdrive
+
 	Joystick m_Joystick;// 2 for shooting and driving 
  
  //  AnalogInput ai0;
@@ -40,50 +31,18 @@ public class Drive_Train extends Robot  {
     Talon frontRight;
     Talon bottomLeft;
     Talon bottomRight;
-    
-    Talon robotFrontLeft;
-    Talon robotFrontRight;
-    Talon robotRearRight;
-   	Talon robotRearLeft;
-    
-   	public Drive_Train() {
-   		robotFrontLeft= new Talon(0);
-   		robotFrontRight= new Talon(3);
-   		robotRearRight = new Talon(2);
-   		robotRearLeft = new Talon(1);
-     
-    	m_Joystick= new Joystick(0);
-<<<<<<< HEAD
-    	robotFrontLeft.setInverted(true);
-    	robotRearLeft.setInverted(true);
-    	m_robotDrive= new RobotDrive(robotFrontLeft,robotRearLeft,robotFrontRight,robotRearRight);
-    }
-   	
-   	public void telopPeriodic() {
-   		double slowDown = 3.0;
-   		double xLeft = m_Joystick.getX();
-   		double yRight =  m_Joystick.getY();
-   		double rightSticktwist = m_Joystick.getTwist();
-			
-   		double gyroAngle = 0;
-   		
-		m_robotDrive.mecanumDrive_Cartesian(xLeft, -yRight, - rightSticktwist, gyroAngle);
-		
-		SmartDashboard.putNumber("xLeft", xLeft);
-		SmartDashboard.putNumber("yRight", yRight);
-		SmartDashboard.putNumber("yLeft", rightSticktwist);
-	}
-=======
-    	
-    	//mecCalc = new HomebrewMecanum();
+
+   public Drive_Train() {
     	frontLeft = new Talon(ContsClass.driveMotorFrontLeft);
     	frontRight = new Talon(ContsClass.driveMotorFrontRight);
     	bottomLeft = new Talon(ContsClass.driveMotorBottomLeft);
-    	bottomRight = new Talon(ContsClass.driveMotorBottomRight);
+    	bottomRight = new Talon(ContsClass.driveMotorBottomRight);	
+     
+    	m_Joystick= new Joystick(0);
+    
     	bottomLeft.setInverted(true);
     	frontLeft.setInverted(true);
     	
-    //	m_robotDrive= new RobotDrive(frontLeft,frontRight,bottomLeft,bottomRight);
     }
 
 public  void telopPeriodic(){
@@ -109,5 +68,4 @@ public  void telopPeriodic(){
 	 
 	 	
 }
->>>>>>> origin/newmecanumdrive
 }
