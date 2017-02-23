6212b0c5ff12ab3f22d5a2b67887fa66701d2fb2
StampedeRobotics <breckstampede@gmail.com>
2017-02-23 16:27:34 -0600

    Revert "winch system added"
    This reverts commit f8a26ab8a736d70d7d783eeb86d72058f081f172.

--- Gears_Headz/src/org/usfirst/frc/team3630/robot/winchSystem.java
+++ Gears_Headz/src/org/usfirst/frc/team3630/robot/winchSystem.java
@@ -4,29 +4,28 @@ import edu.wpi.first.wpilibj.Joystick;
 
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class WinchSystem {
-	private Talon winch;
-	private Joystick m_joystick;
-	private WinchEncoder wEncoder;
-	
-	public WinchSystem(){
-		winch =  new Talon(Consts.winchTalonPWM);
-		m_joystick = new Joystick(Consts.joystickComPort);
-		wEncoder = new WinchEncoder (Consts.winchEncoderPinA, Consts.winchEncoderPinB);
+public class winchSystem {
+	Talon winch;
+	Joystick mecXbox;
+public winchSystem(){
+	 winch =  new Talon(5);
+	 mecXbox = new Joystick(0);
 }
+
+	
 
 	public void upWinch(){
 		winch.set(-1);
 	}
+	
 	
 	public void stopWinch() {
 		winch.set(0.0);
 	}
 	
 	public int onOff() {
-		if (m_joystick.getRawButton(5)){
+		if (mecXbox.getRawButton(5)){
 			return(int) 1;
 		}
 		else{
@@ -34,15 +33,7 @@ public class WinchSystem {
 		}
 	}
 	
-	public void printWinchValues () {
-		SmartDashboard.putNumber("Winch Raw Encoder", wEncoder.getRaw());
-		SmartDashboard.putNumber("Winch Radians Encoder", wEncoder.GetDistRadians());
-		SmartDashboard.putNumber("Winch Degrees Encoder", wEncoder.GetDistDegrees());
-		SmartDashboard.putNumber("Winch Inches Encoder", wEncoder.GetDistInches());
-	}
-	
-	public void teleopPeriodic(){
-		printWinchValues();
+	public void telopMPeriodic(){
 		switch(onOff()) {
 			case 1:
 				upWinch();
