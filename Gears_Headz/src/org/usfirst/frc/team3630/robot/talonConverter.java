b24bee63adb288cb87b2de3d3e51ce34d926ab89
StampedeRobotics <breckstampede@gmail.com>
2017-03-06 17:04:37 -0600

    added wheel pid controllers
    meant to control velocity of wheels so that we can drive straight. - sam
    a

---
+++ Gears_Headz/src/org/usfirst/frc/team3630/robot/talonConverter.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team3630.robot;
+
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.Talon;
+
+public class talonConverter implements PIDOutput {
+	private Talon talon;
+	
+	public talonConverter (Talon t){
+		t = talon;
+	}
+	
+	public void pidWrite(double output) {
+		// TODO Auto-generated method stub
+		output /= 2000;
+		talon.set(output);
+	}
+
+}
