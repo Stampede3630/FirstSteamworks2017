0e60c38cd3ec66bf2fb2dfee8e8e0db4045ffd00
foxnard <foxnard@usfamily.net>
2017-03-14 21:30:40 -0500

    Added Corner class comments
    Can run Javadoc to make use of the new comments.

--- Gears_RoboRealm/src/Corner.java
+++ Gears_RoboRealm/src/Corner.java
@@ -1,11 +1,17 @@
 import java.util.Comparator;
 
+/**
+ * Corner class containing x,y pixel coordinates and angle from blob-centers.
+ */
 public class Corner { // implements Comparable<Corner> {
 
 	private int cornerX;
 	private int cornerY;
 	private double angle; // Angle in radians of point from center of blob. Use for sorting.
 
+	/**
+	 * Construct Corner with default values
+	 */
 	public Corner() {
 		super();
 		this.cornerX = 0;
@@ -13,6 +19,11 @@ public class Corner { // implements Comparable<Corner> {
 		this.angle = 0.0;
 	}
 
+	/**
+	 * Construct Corner with initial X,Y position
+	 * @param cornerX pixel x-coordinate
+	 * @param cornerY pixel y-coordinate
+	 */
 	public Corner(int cornerX, int cornerY) {
 		super();
 		this.cornerX = cornerX;
@@ -20,30 +31,52 @@ public class Corner { // implements Comparable<Corner> {
 		this.angle = 0.0;
 	}
 
+	/**
+	 * @return Corner pixel x-coordinate
+	 */
 	public int getX() {
 		return cornerX;
 	}
+	/**
+	 * @param cornerX set Corner pixel x-coordinate
+	 */
 	public void setX(int cornerX) {
 		this.cornerX = cornerX;
 	}
+	/**
+	 * @return Corner pixel y-coordinate
+	 */
 	public int getY() {
 		return cornerY;
 	}
+	/**
+	 * @param cornerY set Corner pixel y-coordinate
+	 */
 	public void setY(int cornerY) {
 		this.cornerY = cornerY;
 	}
+	/**
+	 * @return Corner angle from blob center in radians
+	 */
 	public double getAngle() {
 		return angle;
 	}
+	
 	public void setAngle(double angle) {
 		this.angle = angle;
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	public String toString() {
 		double tempAngle = Math.round(angle * 100.0) / 100.0;
 		return "(" + Integer.toString(cornerX) + "," + Integer.toString(cornerY) + "/" + Double.toString(tempAngle) + ")";
 	}
 
+	/**
+	 * @return Corner in Coma Separated Variable format
+	 */
 	public String toCSV() {
 		return Integer.toString(cornerX) + "," + Integer.toString(cornerY) + ",";
 	}
@@ -59,6 +92,9 @@ public class Corner { // implements Comparable<Corner> {
 	// 	//return compareX - this.cornerX;
 	// }
 
+	/**
+	 * A comparator to allow sorting corners by pixel x-coordinates.
+	 */
 	public static Comparator<Corner> CornerXComparator
                           = new Comparator<Corner>() {
 
@@ -74,6 +110,9 @@ public class Corner { // implements Comparable<Corner> {
 	      // return cornerX2 - cornerX1;
 	    }
 	};
+	/**
+	 * A comparator to allow sorting corners by angle from blob center.
+	 */
 	public static Comparator<Corner> AngleComparator
                           = new Comparator<Corner>() {
 
