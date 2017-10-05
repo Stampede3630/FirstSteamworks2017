4d7bd60db398fda71b2c8f5119fea9b5a8751966
Sam A <aronsa@student.breckschool.org>
2017-10-05 13:30:23 -0500

    defined interface for wheel

--- Gears_Headz/src/org/usfirst/frc/team3630/robot/Wheel.java
+++ Gears_Headz/src/org/usfirst/frc/team3630/robot/Wheel.java
@@ -1,98 +1,47 @@
 package org.usfirst.frc.team3630.robot;
-import edu.wpi.first.wpilibj.CounterBase.EncodingType;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.PIDSourceType;
 
-public class Wheel {
-	private static final int pulsesPerRevolution = 250;
-	
-	XboxController xbox;
-	public Encoder encoder;
-	public Talon talon;
-	private TalonConverter maxRamp;
-	
-	public PIDController pid;
+ /**
+ * @author robotics
+ *
+ */
 
+public interface Wheel {
+	 /*SpeedController _motorController;
+	 PIDController _pPIDController;
+	 Encoder _encoder;
+	 */
+	 /**
+	  * Sets PID or power mode for motor controller
+	 * @param PIDControl whether to activate PID control or just use power values
+	 */
+	public void setMode (boolean PIDControl);
 	
 	/**
-	 * @param encoderChannel1
-	 * @param encoderChannel2
-	 * @param talonChannel
-	 * @param reversed
+	 * Sets velocity in defined units for specific wheel
+	 * @param velocity desired velocity
 	 */
-	public Wheel (int encoderChannel1, int encoderChannel2, int talonChannel, boolean reversed){
-		xbox = new XboxController(0);
-		encoder = new Encoder(encoderChannel1, encoderChannel2, reversed, EncodingType.k4X);
-		
-		talon = new Talon(talonChannel);		
-		talon.setInverted(reversed);
-		maxRamp = new TalonConverter(talon);
-		encoder.setMaxPeriod(1);
-		// Define distance in terms of inches
-		encoder.setDistancePerPulse(Consts.mecanumWheelRadiusInches*2*Math.PI/pulsesPerRevolution);
-		encoder.setMinRate(10);
-		encoder.setReverseDirection(!reversed);
-		encoder.setSamplesToAverage(7);
-		encoder.setPIDSourceType(PIDSourceType.kDisplacement);
-		
-		//PID WILL ONLY DEAL WITH INCHES/SECONDz
-		pid = new PIDController(
-				SmartDashboard.getNumber("drivetrain kP", Consts.wheelKP),
-				SmartDashboard.getNumber("drivetrain kI", Consts.wheelKI),
-				SmartDashboard.getNumber("drivetrain kD", Consts.wheelKD),
-				encoder, 
-				maxRamp
-				);
-		//pid.setInputRange(-100, 100);
-		pid.setOutputRange(-1, 1);
-		pid.setAbsoluteTolerance(2);
-		pid.enable();
-	}
-	public void setWheelDistance (double distance){
-		//Need to make this constant better.
-		
-		pid.setPID(
-				SmartDashboard.getNumber("drivetrain kP", Consts.wheelKP),
-				SmartDashboard.getNumber("drivetrain kI", Consts.wheelKI),
-				SmartDashboard.getNumber("drivetrain kD", Consts.wheelKD)
-			);
-		
-		pid.setSetpoint(distance);//speed);
-		SmartDashboard.putNumber("PID Encoder Input"+String.valueOf(talon.getChannel()), encoder.pidGet());
-		SmartDashboard.putNumber("PID Setpoint "+String.valueOf(talon.getChannel()), pid.getSetpoint());
-		SmartDashboard.putNumber("PID result"+String.valueOf(talon.getChannel()), pid.get());		
-		
-		}
-
-	public void getInfo () {
-		SmartDashboard.putNumber("PID Encoder Input"+String.valueOf(talon.getChannel()), encoder.pidGet());
-		SmartDashboard.putNumber("PID Setpoint "+String.valueOf(talon.getChannel()), pid.getSetpoint());
-		SmartDashboard.putNumber("PID result"+String.valueOf(talon.getChannel()), pid.get());		
-	}
-	
-	
-	
-
-	public double getDistDegrees()
-	{
-		return encoder.getDistance() * 180.0 / Math.PI; 
-	}
-
-	public double getDistRadians()
-	{
-		return encoder.getDistance(); 
-	}
-
-	/**
-	 * @return encoder distance in inches
-	 */
-	public double getDistInches()
-	{
-		return getDistRadians() * Consts.mecanumWheelRadiusInches;
-	}
-
+	 public void setVelocity (int velocity);
+	 
+	 /**
+	  * gets the position of the encoder
+	  * @return position of the encoder in defined units
+	  */
+	 public int getEncoderPosition ();
+	 /**
+	  * gets the velocity of the encoder
+	  * @return velocity of encoder in defined units (usually units/second)
+	  */
+	 public int getEncoderVelocity ();
+	 /**
+	  * Sets the velocity PID controller. Would reccomend keeping it to defined constants
+	  * @param kP
+	  * @param kI
+	  * @param kD
+	  */
+	 public void setPID (float kP, float kI, float kD);
+	 /**
+	  * Reset the encoder distance
+	  */
+	 public void resetEncoder ();
+	 
 }
