288779ca5901f752ae65e25913e784e30556ab34
aronsa <aronsa@student.breckschool.org>
2017-10-17 11:29:00 -0500

    seperated helper functions

--- robot/src/org/usfirst/frc/team3630/robot/TalonSR.java
+++ robot/src/org/usfirst/frc/team3630/robot/TalonSR.java
@@ -15,101 +15,34 @@ import edu.wpi.first.wpilibj.PIDSourceType;
  *
  */
 public class TalonSR implements Wheel {
-	
+
 	private Encoder _encoder;
 	private Talon _talon;
 	public PIDController pPID;
 	public PIDController vPID;
 	private AltEncoderPID velocityEncoderValues;
 	private VelocityAdjuster _velocityAdjuster;
-	
-	class VelocityAdjuster implements PIDOutput, PIDSource {
-		double vAdjust;
-		double vFeed;
-		
-		public void pidWrite(double output) {
-			vAdjust=output;			
-		}
-		
-		@Override
-		public void setPIDSourceType(PIDSourceType pidSource) {
-			pidSource= PIDSourceType.kRate;	
-		}
-		@Override
-		public PIDSourceType getPIDSourceType() {
-			// TODO Auto-generated method stub
-			return PIDSourceType.kRate;
-		}
-		public void setVDesired(double vAdjust) {
-			vFeed= vAdjust;
-		}
-		public double pidGet() {
-			// need to fix
-			return vFeed+vAdjust;
-		}
-	}
 
-	
-	class  AltEncoderPID implements PIDSource{
+	public TalonSR(int talonPin, int encoderPinA, int encoderPinB, boolean talonReversed, int distPerPulse,
+			boolean encoderReversed) {
 
-		PIDSourceType _PIDSourceType;
-		Encoder _e;
-		/**
-		 * 
-		 * @param e encoder  
-		 * @param pidSource wheather input is a dispacement or a rate 
-		 */
-		public AltEncoderPID(Encoder e, PIDSourceType pidSource){
-			_e=e;
-			_PIDSourceType= pidSource;
-		}
-		@Override
-		public void setPIDSourceType(PIDSourceType pidSource) {
-			
-			_PIDSourceType= pidSource;
-			
-		}
-
-		@Override
-		public PIDSourceType getPIDSourceType() {
-			// TODO Auto-generated method stub
-			return _PIDSourceType;
-		}
-
-		@Override
-		public double pidGet() {
-			// TODO Auto-generated method stub
-			if (PIDSourceType.kDisplacement.equals(_PIDSourceType)) {
-				return _e.getDistance();
-				
-			}
-			else if ( PIDSourceType.kRate.equals(_PIDSourceType)) {
-				return _e.getRate();
-			}
-			else {
-				return -1;
-			}
-		}
-		
-	}
-	
-	public TalonSR (int talonPin, int encoderPinA, int encoderPinB, boolean talonReversed,int distPerPulse, boolean encoderReversed) {
-		
-		_encoder = new Encoder(encoderPinA, encoderPinB, encoderReversed );
-		_encoder.setDistancePerPulse(distPerPulse );
+		_encoder = new Encoder(encoderPinA, encoderPinB, encoderReversed);
+		_encoder.setDistancePerPulse(distPerPulse);
 		_encoder.setPIDSourceType(PIDSourceType.kDisplacement);
-		
+
 		_talon = new Talon(talonPin);
 		_talon.setInverted(talonReversed);
-		
-		velocityEncoderValues= new AltEncoderPID(_encoder,PIDSourceType.kRate);
-		
-		vPID = new PIDController (.05,0,0,1,_velocityAdjuster,_talon);
-		pPID = new PIDController(.05,0,0,1,_encoder,_velocityAdjuster);		
-		
+
+		velocityEncoderValues = new AltEncoderPID(_encoder, PIDSourceType.kRate);
+
+		vPID = new PIDController(.05, 0, 0, 1, _velocityAdjuster, _talon);
+		pPID = new PIDController(.05, 0, 0, 1, _encoder, _velocityAdjuster);
+
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.usfirst.frc.team3630.robot.Wheel#setMode(boolean)
 	 */
 	@Override
@@ -117,14 +50,15 @@ public class TalonSR implements Wheel {
 		if (PIDControl) {
 			vPID.enable();
 			pPID.enable();
-		}
-		else {
+		} else {
 			vPID.disable();
 			pPID.disable();
 		}
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.usfirst.frc.team3630.robot.Wheel#setVelocity(int)
 	 */
 	@Override
@@ -132,7 +66,9 @@ public class TalonSR implements Wheel {
 		_velocityAdjuster.setVDesired(velocity);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.usfirst.frc.team3630.robot.Wheel#getEncoderPosition()
 	 */
 	@Override
@@ -140,7 +76,9 @@ public class TalonSR implements Wheel {
 		return _encoder.getDistance();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.usfirst.frc.team3630.robot.Wheel#getEncoderVelocity()
 	 */
 	@Override
@@ -148,29 +86,34 @@ public class TalonSR implements Wheel {
 		return _encoder.getRate();
 	}
 
-	/* (non-Javadoc)
-	 * @see org.usfirst.frc.team3630.robot.Wheel#setPID(boolean, float, float, float)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.usfirst.frc.team3630.robot.Wheel#setPID(boolean, float, float,
+	 * float)
 	 */
 	@Override
 	public void setPID(boolean position, double kP, double kI, double kD, double kF) {
-		if(position) {
+		if (position) {
 			pPID.setPID(kP, kI, kD, kF);
-		}
-		else {
+		} else {
 			vPID.setPID(kP, kI, kD, kF);
 		}
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.usfirst.frc.team3630.robot.Wheel#resetEncoder()
 	 */
 	@Override
 	public void resetEncoder() {
 		_encoder.reset();
 	}
+
 	@Override
 	public void selfTest() {
-		//TODO: implement this
+		// TODO: implement this
 	}
 
 }
